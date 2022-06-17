1dd86c7e46e51884d4156dbb445d237b6e4c492d
Ayah Elsheikh <ayahhesham0@gmail.com>
2022-06-17 17:43:38 +0200

    add variableand operators file in basics

---
+++ 1-variable&operators.py
@@ -0,0 +1,44 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=5
+x
+5
+x,y=3,5
+x
+3
+y
+5
+x-2=5
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+y=10
+x*y
+30
+9/4
+2.25
+9//4
+2
+type(x)
+<class 'int'>
+print('hello world')
+hello world
+x
+3
+x+=1
+x
+4
+x//2
+2
+x=
+SyntaxError: invalid syntax
+
+x
+4
+x=x+1
+x
+5
+5>=4
+True
+5!=6
+True
+5>6
+False
